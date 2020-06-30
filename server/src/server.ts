2950e871c06ba9404ecdf45aefaa84d3950c36df
gbfragoso <gbfragoso94@gmail.com>
2020-06-30 10:16:08 -0300

    Aula 3 incompleta

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,4 +8,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
-app.listen(3000);
+app.listen(3333);
---
+++ web/src/pages/createpoint/index.tsx
@@ -0,0 +1,163 @@
+import React, {useEffect, useState, ChangeEvent} from 'react';
+import './styles.css';
+import { Link } from 'react-router-dom';
+import { Map, TileLayer, Marker } from 'react-leaflet';
+import api from '../../services/api';
+
+import { FiArrowLeft } from 'react-icons/fi';
+
+import logo from '../../assets/logo.svg';
+import axios from 'axios';
+
+interface Item {
+    id: number;
+    title: string;
+    image_url: string;
+}
+
+interface UF {
+    sigla: string;
+}
+
+interface city {
+    nome: string;
+}
+
+const CreatePoint = () => {
+    const [items, setItems] = useState<Item[]>([]);
+    const [ufs, setUFs] = useState<string[]>([]);
+    const [cities, setCities] = useState<string[]>([]);
+
+    const [selectedUF, setSelectedUF] = useState('0');
+    const [selectedCtiy, setSelectedCity] = useState('0');
+
+    useEffect(() => {
+        api.get('items').then(response => setItems(response.data))
+    }, []);
+
+    useEffect(() => {
+        axios.get<UF[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
+            .then(response => {
+                const ufInitials = response.data.map(uf => uf.sigla);
+                setUFs(ufInitials);
+            });
+    }, []);
+
+    useEffect(() => {
+        if (selectedUF === '0') {
+            return;
+        }
+
+        axios.get<city[]>(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUF}/municipios`)
+            .then(response => {
+                const cities = response.data.map(city => city.nome);
+                setCities(cities);
+            });
+    }, [selectedUF]);
+
+    function handleSelectedUF(event: ChangeEvent<HTMLSelectElement>) {
+        setSelectedUF(event.target.value);
+    }
+
+    function handleSelectedCity(event: ChangeEvent<HTMLSelectElement>) {
+        setSelectedCity(event.target.value);
+    }
+
+    return (
+        <div id="page-create-point">
+            <header>
+                <img src={logo} alt="Ecoleta"/>
+
+                <Link to='/'>
+                    <FiArrowLeft/>
+                    Voltar para home
+                </Link>
+            </header>
+
+            <form >
+                <h1>Cadastro do<br/>ponto de coleta</h1>
+
+                <fieldset>
+                    <legend>
+                        <h2>Dados</h2>
+                    </legend>
+
+                    <div className="field">
+                        <label htmlFor="name">Nome da entidade</label>
+                        <input type="text" name="name" id="name"/>
+                    </div>
+
+                    <div className="field-group">
+                        <div className="field">
+                            <label htmlFor="email">Email</label>
+                            <input type="text" name="email" id="email"/>
+                        </div>
+
+                        <div className="field">
+                            <label htmlFor="whatsapp">Whatsapp</label>
+                            <input type="text" name="whatsapp" id="whatsapp"/>
+                        </div>
+                    </div>
+                </fieldset>
+
+                <fieldset>
+                    <legend>
+                        <h2>Endereço</h2>
+                        <span>Selecione o endereço no mapa</span>
+                    </legend>
+                    
+                    <Map center={[-18.0866433,-39.5506468]} zoom={15}>
+                        <TileLayer
+                            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                        <Marker position={[-18.0866433,-39.5506468]}/>
+                    </Map>
+
+                    <div className="field-group">
+                        <div className="field">
+                            <label htmlFor="uf">Estado (UF)</label>
+                            <select name="uf" id="uf" value={selectedUF} onChange={handleSelectedUF}>
+                                <option value="0">Selecione uma UF</option>
+                                {ufs.map(uf => (
+                                    <option key={uf} value={uf}>{uf}</option>
+                                ))}
+                            </select>
+                        </div>
+                        <div className="field">
+                            <label htmlFor="city">Cidade</label>
+                            <select name="city" id="city" value={selectedCtiy} onChange={handleSelectedCity}>
+                                <option value="0">Selecione uma cidade</option>
+                                {cities.map(city => (
+                                    <option key={city} value={city}>{city}</option>
+                                ))}
+                            </select>
+                        </div>
+                    </div>                    
+                </fieldset>
+
+                <fieldset>
+                    <legend>
+                        <h2>Itens de coleta</h2>
+                        <span>Selecione um ou mais itens abaixo</span>
+                    </legend>
+
+                    <ul className="items-grid">
+                        {items.map(item => (
+                            <li key={item.id}>
+                                <img src={item.image_url} alt={item.title}/>
+                                <span>{item.title}</span>
+                            </li>
+                        ))}
+                    </ul>
+                </fieldset>
+
+                <button type="submit">
+                    Cadastrar ponto de coleta
+                </button>
+            </form>
+        </div>
+    );
+}
+
+export default CreatePoint;
