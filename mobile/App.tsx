f3df11c0e1bcc00c4fc012cd353655d57f7e306b
gbfragoso <gbfragoso94@gmail.com>
2020-07-26 12:00:46 -0300

    updating gitignore

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Oi Gustavo</Text>
+      <Text>Bom Dia</Text>
       <StatusBar style="auto" />
     </View>
   );
